351eab5f44fbeb539ed6364d71a739e5cb2c3d63
Muhammad Naufal Arif Wibowo <37301865+naufaroot@users.noreply.github.com>
2018-03-12 11:50:07 -0400

    Update bla.cpp

--- MuhammadNaufalArifW_0110217020/bla.cpp
+++ MuhammadNaufalArifW_0110217020/bla.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+using namespace std;
+int n;
+
+int faktorial(int n);
+
+int main(){
+  cout<<"Masukkan nilai n = "; cin>>n;
+  cout<<" Faktorial "<<n<<" = "<<faktorial(n)<<endl;
+}
+
+int faktorial(int n){
+  if (n==0){
+      return 1;
+    }
+    else{
+      return (n*faktorial(n-1));
+  }
+}
