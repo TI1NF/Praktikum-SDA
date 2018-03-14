3ccdfb460602d229c91433bc0c5f874135532c77
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2018-03-14 17:43:51 +0700

    Update hoho.cpp

--- AtrisaEndyaNurHidyah_0110217038/hoho.cpp
+++ AtrisaEndyaNurHidyah_0110217038/hoho.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 int main ()
 {
-
-cout << "Cooming Soon" << endl ;
-
-return 0;
-} 
+	int i, no, satu=0, dua=2, next;
+	satu=0;
+	dua=1;
+	cout << "masukan angka pertama "; cin >> no ;
+	cout << "fibonaci nya adalah ";
+	
+	for (i=0; i<no; i++) 
+	{
+		cout << "\n" << satu;
+		next = satu + dua;
+		satu = dua;
+		dua = next;
+	}
+	return 0;
+}
